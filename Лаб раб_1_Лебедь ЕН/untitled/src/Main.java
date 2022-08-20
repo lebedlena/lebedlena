c78a734ad443e59167de45fa44f417a241509f8d
Elena Lebed <111484478+lebedlena@users.noreply.github.com>
2022-08-20 18:41:48 +0300

    Add files via upload

---
+++ Лаб раб_1_Лебедь ЕН/untitled/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введи первое число и нажми ENTER");
+        int a = scan.nextInt();
+        System.out.println("Введи второе число и нажми ENTER");
+        int b = scan.nextInt();
+        System.out.println("Введи третье число и нажми ENTER");
+        int c = scan.nextInt();
+        System.out.println("Твоя сумма:");
+        System.out.println(a+b+c);
+    }
+
+}
